d1cba0b0a04b4083b941c47488ad20e81e49b91d
naimur2001 <naimurrahman18thapril@gmail.com>
2023-12-18 02:37:39 +0600

    hello

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,6 +4,7 @@ const Main = () => {
   return (
     <div>
       <h1  className={`text-3xl text-center text-design ${styles.textDesign}`} >Practice Git command </h1>
+      <h1>Hello</h1>
     </div>
   )
 }
