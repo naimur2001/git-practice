b8aa0fc761b1c2b19cdf715a178c204d2625cc8d
naimur2001 <naimurrahman18thapril@gmail.com>
2023-12-18 16:02:11 +0600

    section1 updated

--- src/Components/Developers/section1.jsx
+++ src/Components/Developers/section1.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const section1 = () => {
   return (
+    <>
     <div>section1</div>
+    <h1>I created a new Branch.
+      Name: Developer.
+    </h1>
+    </>
   )
 }
 
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './styles.module.css';
+import section1 from './Developers/section1';
 const Main = () => {
   return (
 
 
     <div>
       <h1  className={`text-3xl text-center text-design ${styles.textDesign}`} >Practice Git command </h1>
-     
+    <section1></section1>
     </div>
 
   )
